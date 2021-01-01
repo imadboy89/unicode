a78bc04e2253208172f5a6d1d417075107c89d7b
Imad EL Kholti <im.el-kholti@domain.com>
2021-01-01 17:57:45 +0100

    Updates

---
+++ precache-manifest.d701f6ad7e4b4cab9b5856b8703ced53.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7067ceeef4a335cc4a0a",
+    "url": "/unicode/static/js/app.4b2b5e49.chunk.js"
+  },
+  {
+    "revision": "62f60001a036a159ff04",
+    "url": "/unicode/static/js/runtime~app.83ddfce1.js"
+  },
+  {
+    "revision": "6ebca74f0a18771e97bd",
+    "url": "/unicode/static/js/2.9260fdcf.chunk.js"
+  },
+  {
+    "revision": "066950d47b6cd56cc42da5392ec2150f",
+    "url": "/unicode/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/unicode/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/unicode/expo-service-worker.js"
+  },
+  {
+    "revision": "c71c509da520639a42f995c1729fe699",
+    "url": "/unicode/register-service-worker.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/unicode/static/js/2.9260fdcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/unicode/favicon-16.png"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/unicode/favicon-32.png"
+  },
+  {
+    "revision": "50b1bd72d8b0c569ffcad83d622ec176",
+    "url": "/unicode/favicon.ico"
+  },
+  {
+    "revision": "3a344eaa162ab6b2d2906c490775bdef",
+    "url": "/unicode/manifest.json"
+  }
+];
